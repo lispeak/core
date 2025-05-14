eca6ddd058e2749206b016a183d31bf4821792cf
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-05-14 23:24:03 +0400

    Remove trash

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import complex from "./modules/complex";
-import primitives from "./modules/primitives";
-
-console.log(primitives);
-console.log(complex);
+console.log("Hello Lispeak!");
--- src/modules/roots.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  truth: { consonants: "b" },
-  many: { consonants: "m" },
-  time: { consonants: "l" },
-};
