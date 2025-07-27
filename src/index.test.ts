dff9ef03d022fcd1c8e1d9ba682d414f5eb8c4e8
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-07-27 18:17:30 +0400

    Change to parser

---
+++ src/index.test.ts
@@ -0,0 +1,4 @@
+import parse from './index';
+
+const source = `(a b c)`;
+parse(source);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import parse from './parser';
+import grammar from './grammar';
+import semantics from './semantics';
 
-const source = `a`;
-parse(source);
+const parse = (source: string) => {
+    const matchResult = grammar.match(source);
+    if (!matchResult.succeeded()) {
+        console.log('Failed to match');
+        console.log(matchResult.message);
+        return;
+    }
+    console.log('Successfully matched');
+
+    try {
+        const ast = semantics(matchResult).ast();
+        console.log('Successfully parsed');
+        console.log(ast);
+    } catch (error) {
+        console.log('Failed to parse');
+        console.log(error);
+    }
+};
+
+export default parse;
