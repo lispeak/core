f54eab55d4e16dcb82e01422279d19ed9f8d617a
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2025-07-27 17:18:53 +0400

    Add ohm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+import * as ohm from 'ohm-js';
+
+const grammar = ohm.grammar(`
+Lispeak { 
+  program = (space* expr space*)* 
+
+  expr = char+ | number | list
+  list =  "(" space? expr (space+ expr)* space? ")"
+
+  number = digit+
+  char  = letter | digit | "-" | "~" | ">" | "<" 
+}
+`);
+
+const source = `
+(a b (c d) e)
+(b c d)
+(1 2 3) (4 5 6)
+`;
+
+const matchResult = grammar.match(source);
+console.log(matchResult.succeeded());
+console.log(matchResult.message);
